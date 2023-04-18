59c345856d6d9b36bc39686e36bc45a44dd0ee92
AxelFunes <chakyfunes@gmail.com>
2023-04-18 17:34:50 -0300

    Agregar archivos de proyecto.

---
+++ TP3 Sim/Form2.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Class Form2
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        InitializeComponent()
+
+    End Sub
+    Private Sub btnInicio_Click(sender As Object, e As EventArgs) Handles btnInicio.Click
+        Dim ventana As New Form1
+        Form1.ShowDialog()
+        Me.Hide()
+    End Sub
+End Class
